a2b6187b35dcbcc1f6bcea6b748310b670ef8617
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2024-05-07 15:56:00 -0300

    api e banco de dados

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,9 +1,25 @@
+import {useEffect, useState} from "react"
+
 import styles from "./ProjectForm.module.css"
 import Input from "../form/Input"
 import Select from "../form/Select"
 import SubmitButton from "../form/SubmitButton"
 
 function ProjectForm ({btnText}) {
+    const [categories, setCategories] = useState([])
+  useEffect(()=>{
+    fetch ('http://localhost:5000/categories',{
+        method: "GET",
+        headers:{
+            "Content-Type":"aplication/json",
+        },
+    })
+    .then((resp)=>resp.json())
+    .then((data)=>{
+        setCategories(data)
+    })
+    .catch((err)=>console.log(err))
+  },[])
     return(
         <form className={styles.form}>
         <div>
@@ -12,7 +28,7 @@ function ProjectForm ({btnText}) {
         <div>
         <Input type="number" text="Orçamento do Projeto" name="budget" placeholder="Insira o orçamento total"/>
         </div>
-        <Select name="category_id" text="Selecione a categoria"/>
+        <Select name="category_id" text="Selecione a categoria" options={categories}/>
         <SubmitButton text={btnText}/>
         </form>
     )
